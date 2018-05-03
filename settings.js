219c623aed14796d72f628b32d7bb1b20cc8ff51
Neil Mistry <neil@amplience.com>
2018-05-03 17:50:34 +0100

    updates

--- settings.js
+++ settings.js
@@ -5,28 +5,57 @@ module.exports = {
             route: "/",
             layout: "landing",
             slots: {
-                "content": "3cbba4da-f90e-47f5-9457-cb70a6cd7770"
+                "content": "8d9bf95a-f0df-4a72-871a-59411ef0d9ec"
             }
         },
         {
-            route: "/bose",
+            route: "/womens",
             layout: "landing",
             slots: {
-                "content": "8e5a7412-ad68-4125-a350-3e35d0c9d1ee"
+                "content": "31e910c1-66bf-4d49-addd-f3c2a83b3612"
             }
         },
         {
-            route: "/kitchen",
+            route: "/mens",
             layout: "landing",
             slots: {
-                "content": "2695b577-f0a0-458f-b516-d3a1bde5ea71"
+                "content": "63bc2b2a-87ac-48a2-89fc-5f66be2d82b3"
             }
         },
         {
-            route: "/mac",
+            route: "/beauty",
             layout: "landing",
             slots: {
-                "content": "f1acfa8f-1fde-42ea-912f-d8e786bb71cd"
+                "content": "b65dc0d6-3bdd-4874-9d55-70508809f560"
+            }
+        },
+        {
+            route: "/lighting",
+            layout: "landing",
+            slots: {
+                "content": "56b081e0-b225-4c78-8162-c6fc7007702f"
+            }
+        },
+        {
+            route: "/furniture",
+            layout: "landing",
+            slots: {
+                "content": "6aeb8278-6620-4286-96c6-7b8d9fcfcee4"
+            }
+        },
+        ,
+        {
+            route: "/electricals",
+            layout: "landing",
+            slots: {
+                "content": "ccc0338f-8e11-42e6-83f7-6c8ee09626fc"
+            }
+        },
+        {
+            route: "/inspiration",
+            layout: "landing",
+            slots: {
+                "content": "83c6e4c9-7e30-4db2-8f6c-3ffefd0bca2f"
             }
         }
     ]
