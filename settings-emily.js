09a7de5147676ff154920c6e34fde9056d05593d
Neil Mistry <neil@amplience.com>
2018-06-01 09:55:27 +0100

    Updated before manual changes for quiz

---
+++ settings-emily.js
@@ -0,0 +1,62 @@
+module.exports = {
+    cms: "https://c1.adis.ws",
+    cmsAccount: "presales",
+    sitemap: [{
+            route: "/",
+            layout: "landing",
+            slots: {
+                "content": "70a2dc02-c774-4d57-89d8-cd23afa04192"
+            }
+        },
+        {
+            route: "/womens",
+            layout: "landing",
+            slots: {
+                "content": "77b984af-43fd-4806-b823-9b9a6a56b8c1"
+            }
+        },
+        {
+            route: "/mens",
+            layout: "landing",
+            slots: {
+                "content": "63d92da3-6e56-4ff1-ac13-5a839d0ab0ca"
+            }
+        },
+        {
+            route: "/beauty",
+            layout: "landing",
+            slots: {
+                "content": "7e952515-8f2a-4535-a863-7b96ccb1e626"
+            }
+        },
+        {
+            route: "/lighting",
+            layout: "landing",
+            slots: {
+                "content": "76f6ef64-6fb2-4a65-b510-eb3e80374264"
+            }
+        },
+        {
+            route: "/furniture",
+            layout: "landing",
+            slots: {
+                "content": "5db62a63-00dc-4fe4-8083-11f5941135be"
+            }
+        },
+        ,
+        {
+            route: "/electricals",
+            layout: "landing",
+            slots: {
+                "content": "7e6e0cc9-e103-4d85-97fa-0dcce6240a9d"
+            }
+        },
+        {
+            route: "/inspiration",
+            layout: "landing",
+            slots: {
+                "content": "b4b6b213-79bd-4533-bef7-b10aa9393780"
+            }
+        }
+    ]
+};
