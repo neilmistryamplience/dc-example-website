55a763981f17f108731f4c86f8158032e0ae33b1
Emily Godfrey <egodfrey@amplience.com>
2019-02-04 15:38:01 +0000

    Added a hard coded en-GB locale to query parameter

--- cms/services/ContentDeliveryClient.js
+++ cms/services/ContentDeliveryClient.js
@@ -29,7 +29,8 @@ ContentDeliveryClient.prototype.query = function(query, scope, fullBodyObject) {
             '?query=' + encodeURIComponent(JSON.stringify(query)) +
             '&store=' + encodeURIComponent(this.account) +
             '&scope=' + encodeURIComponent(scope) +
-            '&fullBodyObject=' + encodeURIComponent(fullBodyObject);
+            '&fullBodyObject=' + encodeURIComponent(fullBodyObject) +
+            '&locale=en-GB';
     console.log(url)
 
     return new Promise(function(resolve, reject) {
